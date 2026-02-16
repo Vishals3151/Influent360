b9f8d1cdcb4a8d134a8841c4ddfb54c2a280de5a
Vishal Gaikwad <vishalsureshgaikwad115@gmail.com>
2026-02-16 19:17:03 +0530

    Update backend/util/SecretToken.js
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- backend/util/SecretToken.js
+++ backend/util/SecretToken.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const { TOKEN_KEY } = process.env;
+if (!TOKEN_KEY) {
+  throw new Error("TOKEN_KEY environment variable is not set");
+}
 
 module.exports.createSecretToken = (id) => {
   return jwt.sign({ id }, TOKEN_KEY, {
